66f3d05d52153b068c5d9415a335e26ddb06970d
wk-j <somnuk.wk@gmail.com>
2019-02-07 14:48:22 +0700

    Camel dict

---
+++ src/CamelCase/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace CamelCase {
+    class Model {
+        public string Name { set; get; }
+        public string Value { set; get; }
+        public Dictionary<string, string> Properties { set; get; } = new Dictionary<string, string>();
+    }
+    class Program {
+        static void Main(string[] args) {
+            var obj = new Model();
+            obj.Name = "Name";
+            obj.Value = "Value";
+            obj.Properties.Add("A:A", "V:V");
+
+            var json = JsonConvert.SerializeObject(obj, new JsonSerializerSettings {
+                ContractResolver = new CamelCasePropertyNamesContractResolver()
+            });
+            Console.WriteLine(json);
+        }
+    }
+}
